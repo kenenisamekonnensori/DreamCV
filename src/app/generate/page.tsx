ef701e7d3cd98a1ab23ebddfb64093113120e181
Kenenisa Mekonnen <mekonnenkenenisa2@gmail.com>
2026-01-23 20:07:01 +0300

    made generate page dynamic

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import ResumeFormPage from "@/components/resume-form/ResumeFormPage";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Generate Resume",
   description: "Generate your resume with a selected template.",
